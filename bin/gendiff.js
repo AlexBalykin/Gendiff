2ec6dc480e9ddd2db5fb0025d64b65a86d28a2af
Aleba <alexander.balykin1@gmail.com>
2020-09-27 00:30:05 +0300

    added format

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -4,6 +4,8 @@ import program from 'commander';
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
-  .helpOption('-h, --help', 'output usage information');
+  .helpOption('-h, --help', 'output usage information')
+  .option('-f, --format [type]', 'output format')
+  .arguments('<filepath1> <filepath2>');
 
 program.parse(process.argv);

5db45b66add385c463775d6246245a648b85625c
Aleba <alexander.balykin1@gmail.com>
2020-10-04 01:09:50 +0300

    +action

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -1,11 +1,15 @@
 #!/usr/bin/env node
 import program from 'commander';
+import gendiff from '../index.js';
 
 program
   .version('1.0.0')
   .description('Compares two configuration files and shows a difference.')
   .helpOption('-h, --help', 'output usage information')
   .option('-f, --format [type]', 'output format')
-  .arguments('<filepath1> <filepath2>');
+  .arguments('<filepath1> <filepath2>')
+  .action((file1, file2) => {
+    gendiff(file1, file2);
+  });
 
 program.parse(process.argv);

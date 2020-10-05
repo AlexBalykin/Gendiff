1029eb3c880ea2ad4f6e4ab956f8f73eaf559b39
Aleba <alexander.balykin1@gmail.com>
2020-10-05 13:40:45 +0300

    +folder src

---
+++ src/index.js
@@ -0,0 +1,28 @@
+import _ from 'lodash';
+import fs from 'fs';
+import path from 'path';
+
+export default (json1, json2) => {
+  const file1 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), json1), 'utf8'));
+  const file2 = JSON.parse(fs.readFileSync(path.resolve(process.cwd(), json2), 'utf8'));
+
+  const unionKeys = _.union(_.keys(file1), _.keys(file2));
+
+  const dif = unionKeys.sort().map((keys) => {
+    if (!_.has(file1, keys)) {
+      return `+ ${keys}: ${file2[keys]}`;
+    }
+    if (!_.has(file2, keys)) {
+      return `- ${keys}: ${file1[keys]}`;
+    }
+    if (file2[keys] === file1[keys]) {
+      return `  ${keys}: ${file2[keys]}`;
+    }
+    const dif1 = `- ${keys}: ${file1[keys]}\n+ ${keys}: ${file2[keys]}`;
+    return [dif1];
+  });
+  const result = `{\n${dif.join('\n')}\n}`;
+
+  console.log(result);
+  return result;
+};

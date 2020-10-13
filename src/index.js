0139d59fc70877111f4173960a1f8aa1ddb6e71a
Aleba <alexander.balykin1@gmail.com>
2020-10-14 01:05:59 +0300

    +tree

--- src/index.js
+++ src/index.js
@@ -1,29 +1,19 @@
-import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
+import getAst from './ast.js';
 
-export default (data1, data2) => {
-  const file1 = parse(fs.readFileSync(path.resolve(data1), 'utf8'),
-    path.extname(data1).slice(1));
-  const file2 = parse(fs.readFileSync(path.resolve(data2), 'utf8'),
-    path.extname(data2).slice(1));
+const gendiff = (data1, data2) => {
+  const file1 = parse(
+    fs.readFileSync(path.resolve(data1), 'utf8'),
+    path.extname(data1).slice(1),
+  );
+  const file2 = parse(
+    fs.readFileSync(path.resolve(data2), 'utf8'),
+    path.extname(data2).slice(1),
+  );
 
-  const unionKeys = _.union(_.keys(file1), _.keys(file2));
-
-  const dif = unionKeys.sort().map((key) => {
-    if (!_.has(file1, key) && _.has(file2, key)) {
-      return `  + ${key}: ${file2[key]}`;
-    }
-    if (!_.has(file2, key) && _.has(file1, key)) {
-      return `  - ${key}: ${file1[key]}`;
-    }
-    if (_.isEqual(file2[key], file1[key])) {
-      return `    ${key}: ${file2[key]}`;
-    }
-    return `  - ${key}: ${file1[key]}\n  + ${key}: ${file2[key]}`;
-  });
-  const result = `{\n${dif.join('\n')}\n}`;
-
-  return result;
+  return getAst(file1, file2);
 };
+
+export default gendiff;

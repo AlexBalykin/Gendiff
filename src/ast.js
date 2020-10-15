f32c0ccd4452a5de953e37f5388d99a97d02fe0e
Aleba <alexander.balykin1@gmail.com>
2020-10-15 23:46:27 +0300

    +tree

--- src/ast.js
+++ src/ast.js
@@ -5,16 +5,32 @@ const getAst = (file1, file2) => {
 
   const ast = unionKeys.sort().map((key) => {
     if (!_.has(file1, key) && _.has(file2, key)) {
-      return { key, status: 'added', value: file2[key] };
+      return {
+        key,
+        status: 'added',
+        value: file2[key],
+      };
     }
     if (!_.has(file2, key) && _.has(file1, key)) {
-      return { key, status: 'removed', value: file1[key] };
+      return {
+        key,
+        status: 'removed',
+        value: file1[key],
+      };
     }
     if (_.isEqual(file2[key], file1[key])) {
-      return { key, status: 'unchanged', value: file2[key] };
+      return {
+        key,
+        status: 'unchanged',
+        value: file2[key],
+      };
     }
     if (_.isObject(file1[key]) && _.isObject(file2[key])) {
-      return getAst(file1[key], file2[key]);
+      return {
+        key,
+        status: 'head',
+        ast: getAst(file1[key], file2[key]),
+      };
     }
     return {
       key,
--- src/index.js
+++ src/index.js
@@ -1,3 +1,4 @@
+import _ from 'lodash';
 import path from 'path';
 import fs from 'fs';
 import parse from './parsers.js';
@@ -6,14 +7,42 @@ import getAst from './ast.js';
 const gendiff = (data1, data2) => {
   const file1 = parse(
     fs.readFileSync(path.resolve(data1), 'utf8'),
-    path.extname(data1).slice(1),
+    path.extname(data1).slice(1)
   );
   const file2 = parse(
     fs.readFileSync(path.resolve(data2), 'utf8'),
-    path.extname(data2).slice(1),
+    path.extname(data2).slice(1)
   );
 
-  return getAst(file1, file2);
-};
+  const tree = getAst(file1, file2);
 
-export default gendiff;
+  const diff = (obj) => {
+    const space = '  ';
+    const keys = Object.keys(obj);
+
+    const result = keys.flatMap((i) => {
+      if (typeof obj[i] === 'object') {
+        diff(obj[i]);
+      }
+
+      if (obj[i] === 'added') {
+        console.log(`${space.repeat(3)}+ ${obj.key}:`, obj.value);
+      }
+      if (obj[i] === 'removed') {
+        console.log(`${space.repeat(3)}- ${obj.key}:`, obj.value);
+      }
+      if (obj[i] === 'unchanged') {
+        console.log(`${space.repeat(3)}  ${obj.key}:`, obj.value);
+      }
+      if (obj[i] === 'head') {
+        console.log(`${space.repeat(2)}${obj.key}: {`);
+      }
+      if (obj[i] === 'changed') {
+        console.log(`${space.repeat(3)}- ${obj.key}:`, obj.oldValue);
+        console.log(`${space.repeat(3)}+ ${obj.key}:`, obj.newValue);
+      }
+    });
+  };
+  diff(tree);
+};
+console.log(gendiff('q.json', 'w.json'));

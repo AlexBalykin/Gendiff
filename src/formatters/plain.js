43e98b50b109699ec0c665c0f6e708711a830f72
Aleba <alexander.balykin1@gmail.com>
2020-10-24 22:37:05 +0300

    plain formatter

--- src/formatters/plain.js
+++ src/formatters/plain.js
@@ -7,23 +7,19 @@ const stringify = (value) => {
   return !_.isObject(value) ? `'${value}'` : '[complex value]';
 };
 
-const plain = (arr) => {
+const plain = (arr, path = '') => {
   const result = arr.map((i) => {
-    if (i.status === 'head') {
-      return `${plain(i.ast)}`;
-    }
-    if (i.status === 'added') {
-      return `Property '${i.key}' was added with value: ${stringify(i.value)}`;
-    }
-    if (i.status === 'removed') {
-      return `Property '${i.key}' was removed`;
-    }
-    if (i.status === 'unchanged') {
-      return '';
-    }
-    return `Property '${i.key}' was updated. From ${stringify(
-      i.oldValue,
-    )} to ${stringify(i.newValue)}`;
+    const fullPath = `${path}${i.key}`;
+
+    const obj = {
+      head: () => plain(i.ast, `${path}${i.key}.`),
+      added: () => `Property '${fullPath}' was added with value: ${stringify(i.value)}`,
+      removed: () => `Property '${fullPath}' was removed`,
+      unchanged: () => '',
+      changed: () => `Property '${fullPath}' was updated. From ${stringify(i.oldValue)} to ${stringify(i.newValue)}`,
+    };
+
+    return obj[i.status]();
   });
 
   return result.filter((i) => /\S/.test(i)).join('\n');

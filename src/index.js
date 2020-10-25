f0ee67b100f86bd6bb224e2225e46fcaa3939eaf
Aleba <alexander.balykin1@gmail.com>
2020-10-26 00:35:55 +0300

    -duplication

--- src/index.js
+++ src/index.js
@@ -4,16 +4,12 @@ import parse from './parsers.js';
 import getAst from './ast.js';
 import getFormat from './formatters/index.js';
 
-export default (data1, data2, format = 'stylish') => {
-  const file1 = parse(
-    fs.readFileSync(path.resolve(data1), 'utf8'),
-    path.extname(data1).slice(1),
-  );
-  const file2 = parse(
-    fs.readFileSync(path.resolve(data2), 'utf8'),
-    path.extname(data2).slice(1),
-  );
+const getPath = (str) => fs.readFileSync(path.resolve(str), 'utf8');
+const getExtname = (str) => path.extname(str).slice(1);
 
+export default (data1, data2, format = 'stylish') => {
+  const file1 = parse(getPath(data1), getExtname(data1));
+  const file2 = parse(getPath(data2), getExtname(data2));
   const tree = getAst(file1, file2);
 
   return getFormat(tree, format);

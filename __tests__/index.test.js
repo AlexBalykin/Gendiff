fff2a884fd0e133aa67d2323860b39647a07bdb5
Aleba <alexander.balykin1@gmail.com>
2021-02-01 23:04:55 +0300

    async

--- __tests__/index.test.js
+++ __tests__/index.test.js
@@ -1,20 +1,16 @@
-import path, { dirname } from 'path';
-import { fileURLToPath } from 'url';
-import fs from 'fs';
+import path from 'path';
+import { promises as fs } from 'fs';
 import gendiff from '../src/index.js';
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = dirname(__filename);
-
-const getPath = (filename) => path.join(__dirname, '..', '__tests__', '__fixtures__', filename);
+const getPath = (filename) => path.join('__tests__', '__fixtures__', filename);
 
 test.each([
   ['file1.json', 'file2.json', 'stylish', 'stylish.txt'],
   ['file3.yml', 'file4.yml', 'plain', 'plain.txt'],
   ['file5.ini', 'file6.ini', 'json', 'json.txt'],
-])('.add(%s)', (filename1, filename2, formatter, equal) => {
+])('%s', async (filename1, filename2, formatter, equal) => {
   const data1 = getPath(filename1);
   const data2 = getPath(filename2);
-  const equalFormat = fs.readFileSync(getPath(equal), 'utf8');
+  const equalFormat = await fs.readFile(getPath(equal), 'utf8');
   expect(gendiff(data1, data2, formatter)).toEqual(equalFormat);
 });
